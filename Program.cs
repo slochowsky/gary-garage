543d4586999ce4457d40bc306692804f20367356
blake slochowsky <blakeslochowsky.com>
2020-06-04 13:03:19 -0500

    vrooom

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GaryGarage
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cessna plane = new Cessna () {
+                MainColor = "Blue",
+                MaximumOccupancy = "6",
+                FuelCapacity = 22.2
+            };
+
+            Ram truck = new Ram () {
+                MainColor = "Red",
+                MaximumOccupancy = "20",
+                FuelCapacity = 40.5
+            };
+
+            truck.Drive ();
+        }
+    }
+}
---
+++ Zero.cs
@@ -0,0 +1,8 @@
+public class Zero : Vehicle // Electric motorcycle
+{
+    public double BatteryKWh { get; set; }
+    public void ChargeBattery()
+    {
+        // method definition omitted
+    }
+}
